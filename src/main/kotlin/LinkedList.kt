661dc3ae37b2d99e8a743167ad6bd087552000b9
BMeyer-18 <bzmeyer18@gmail.com>
2026-09-12 13:25:23 -0400

    implemented LinkedList class

---
+++ src/main/kotlin/LinkedList.kt
@@ -0,0 +1,76 @@
+package org.linked_data_structures
+
+/**
+ * A class for a doubly linked list to contain elements of type T.
+ */
+class LinkedList<T> {
+    data class Node<T>(var data: T, var next: Node<T>?, var last: Node<T>?)
+
+    var head: Node<T>? = null
+    var tail: Node<T>? = null
+
+    /**
+     * Adds the element [data] to the front of the list
+     */
+    fun pushFront(data: T) {
+        val newNode = Node(data, head, null)
+        head = newNode
+        if (tail == null)
+            tail = newNode
+    }
+
+    /**
+     * Adds the element [data] to the back of the linked list
+     */
+    fun pushBack(data: T) {
+        val newNode = Node(data, null, tail)
+        tail = newNode
+        if (head == null)
+            head = newNode
+    }
+
+    /**
+     * Removes an element from the front of the list. If the list is empty, it is unchanged.
+     * @return the value at the front of the list or nil if none exists
+     */
+    fun popFront(): T? {
+        val oldHead = head
+        head = oldHead?.next
+        if (head == null)
+            tail = null
+        return oldHead?.data
+    }
+
+    /**
+     * Removes an element from the back of the list. If the list is empty, it is unchanged.
+     * @return the value at the back of the list or nil if none exists
+     */
+    fun popBack(): T? {
+        val oldTail = tail
+        tail = oldTail?.last
+        if (tail == null)
+            head = null
+        return oldTail?.data
+    }
+
+    /**
+     * @return the value at the front of the list or nil if none exists
+     */
+    fun peekFront(): T? {
+        return head?.data
+    }
+
+    /**
+     * @return the value at the back of the list or nil if none exists
+     */
+    fun peekBack(): T? {
+        return tail?.data
+    }
+
+    /**
+     * @return true if the list is empty and false otherwise
+     */
+    fun isEmpty(): Boolean {
+        return (head == null && tail == null)
+    }
+}
